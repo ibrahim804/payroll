18d16eb4c742d88e879080cf92da31679f4da342
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-10 17:29:40 +0600

    users table updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,26 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->string('nationality')->default('bangladeshi');
+            $table->string('passport_number');
+            $table->string('photo_path');
+            $table->text('personal_address');
+            $table->string('city');
+            $table->string('phone');
+            $table->integer('designation_id');
+            $table->integer('department_id');
+            $table->integer('salary_id');
+            $table->integer('working_days_id');
+            $table->date('joining_date');
+            $table->boolean('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
