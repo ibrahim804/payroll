25c00ca3d61c2ece0c3a9f709602f11445ae30db
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-11 14:10:58 +0600

    Payment files added

---
+++ app/Payment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    //
+
+    protected $fillable = [
+        'user_id',
+        'salary_id',
+        'employee_monthly_cost',
+        'payable_amount',
+        'payment_date',
+    ];
+}
