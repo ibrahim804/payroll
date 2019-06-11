88bdc721bb257968c55994634155101394c2f27f
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-11 14:50:52 +0600

    Leave_category filed added

---
+++ app/Http/Controllers/LeaveCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Leave_category;
+use Illuminate\Http\Request;
+
+class LeaveCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Leave_category  $leave_category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Leave_category $leave_category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Leave_category  $leave_category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Leave_category $leave_category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Leave_category  $leave_category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Leave_category $leave_category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Leave_category  $leave_category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Leave_category $leave_category)
+    {
+        //
+    }
+}
