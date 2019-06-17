3f8bc097cc5e5b55e8b556455ed03f257b71ebdb
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-17 14:22:42 +0600

    one to many with users

--- app/Department.php
+++ app/Department.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\User;
 
 class Department extends Model
 {
@@ -12,4 +13,9 @@ class Department extends Model
     protected $fillable = [
         'department_name',
     ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
