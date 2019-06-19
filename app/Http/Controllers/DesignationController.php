92d5b556ba23f4acc914c1cc16b310d8a81f265c
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-19 18:39:06 +0600

    index store and show added

--- app/Http/Controllers/DesignationController.php
+++ app/Http/Controllers/DesignationController.php
@@ -4,82 +4,94 @@ namespace App\Http\Controllers;
 
 use App\Designation;
 use Illuminate\Http\Request;
+use App\Http\Controllers\CustomsErrorsTrait;
+use App\Department;
 
 class DesignationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    use CustomsErrorsTrait;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api'); //->except(['register', 'login']);
+    }
+
     public function index()
     {
-        //
+        $designations = Designation::all();
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'designations' => $designations,
+            ]
+        ];
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to create Designation');
+
+        $validate_attributes = $this->validateDesignation();
+
+        if(Department::findOrFail((int)$validate_attributes['department_id'])->designations->where('designation', $validate_attributes['designation'])->count())
+        {
+            return $this->getErrorMessage('This department already has this designation');
+        }
+
+        $designation = Designation::create($validate_attributes);
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'department_name' => $designation->department->department_name,
+                'designation' => $designation->designation,
+            ]
+        ];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Designation  $designation
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Designation $designation)
+    public function show($id)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to view specific Designation');
+
+        $designation_name = Designation::findOrFail($id)->designation;
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'designation' => $designation_name,
+            ]
+        ];
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Designation  $designation
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Designation $designation)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Designation  $designation
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Designation $designation)
     {
-        //
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Designation  $designation
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Designation $designation)
     {
         //
     }
+
+    private function validateDesignation()
+    {
+        return request()->validate([                           // For DATABASE Validation
+            'department_id' => 'required|string',
+            'designation' => 'required|string',
+        ]);
+    }
 }
+
+
+
+
+
+
+
+
+
+//
