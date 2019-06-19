136f6085905bcc2216296b32533904475e30b3b9
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-19 15:57:13 +0600

    one to many with users, many to one with department

--- app/Designation.php
+++ app/Designation.php
@@ -3,10 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Department;
 
 class Designation extends Model
 {
     //
 
     protected $fillable = ['designation'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
