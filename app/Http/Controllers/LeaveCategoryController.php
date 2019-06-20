f6d72a2942155362a506e93945a4dec785e168ac
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-20 14:24:33 +0600

    leave category show and update added.

--- app/Http/Controllers/LeaveCategoryController.php
+++ app/Http/Controllers/LeaveCategoryController.php
@@ -48,14 +48,41 @@ class LeaveCategoryController extends Controller
         ];
     }
 
-    public function show(Leave_category $leave_category)
+    public function show($id)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to create leave category');
+
+        $leave_type = Leave_category::findOrFail($id)->leave_type;
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'leave_type' => $leave_type,
+            ]
+        ];
     }
 
-    public function update(Request $request, Leave_category $leave_category)
+    public function update(Request $request, $id)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to update Leave Category');
+
+        $leave_category = Leave_category::findOrFail($id);
+        $validator = $this->validateLeaveCategory($request);
+
+        if($validator->fails()) return $this->getErrorMessage($validator->errors());
+
+        $old_leave_category = $leave_category->leave_type;
+        $leave_category->update($request->all());
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'old_leave_category' => $old_leave_category,
+                'new_leave_category' => $leave_category->leave_type,
+            ]
+        ];
     }
 
     public function destroy(Leave_category $leave_category)
