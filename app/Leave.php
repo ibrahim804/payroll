edcb07081af0e67cbe660b41f1fd4ec901a299d6
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-20 12:44:48 +0600

    many to one relation with Leave category

--- app/Leave.php
+++ app/Leave.php
@@ -3,13 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Leave_category;
 
 class Leave extends Model
 {
     //
 
     protected $fillable = [
-        'user_id',
+        //'user_id',
         'leave_category_id',
         'leave_description',
         'application_date',
@@ -17,4 +18,9 @@ class Leave extends Model
         'end_date',
         'approval_status',
     ];
+
+    public function leave_category()
+    {
+        return $this->belongsTo(Leave_category::class);
+    }
 }
