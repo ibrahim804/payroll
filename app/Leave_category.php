2d34aa9eabc361b6d5d06bdbf1264e6c8d596cbb
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-20 12:46:25 +0600

    one to many relation with Leave

--- app/Leave_category.php
+++ app/Leave_category.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Leave;
 
 class Leave_category extends Model
 {
     //
 
     protected $fillable = ['leave_type'];
+
+    public function leaves()
+    {
+        return $this->hasMany(Leave::class);
+    }
 }
