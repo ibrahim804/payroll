e6728ac70a545a0adc42a11762f2c0ef317064d7
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-20 16:49:08 +0600

    index, myleaves, store, validator added.
    month wise query through index, any user can watch his all leaves

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -4,82 +4,107 @@ namespace App\Http\Controllers;
 
 use App\Leave;
 use Illuminate\Http\Request;
+use App\Http\Controllers\CustomsErrorsTrait;
+use Validator;
 
 class LeaveController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    use CustomsErrorsTrait;
+
+    public function __construct()
     {
-        //
+        $this->middleware('auth:api'); //->except(['register', 'login']);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function index($month)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to view all leaves');
+
+        $leaves = Leave::where('month', $month)->get();
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'leaves' => $leaves,
+            ]
+        ];
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    public function myLeaves()
+    {
+        $myLeaves = auth()->user()->leaves;
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'myLeaves' => $myLeaves,
+            ]
+        ];
+    }
+
     public function store(Request $request)
     {
-        //
+        $validator = $this->validateLeave($request);
+
+        if($validator->fails()) return $this->getErrorMessage($validator->errors());
+
+        $inputs = $request->all();
+        $inputs['user_id'] = auth()->id();
+        $inputs['application_date'] = date("Y-m-d H:i:s", strtotime('+6 hours'));
+        $inputs['month'] = date("M", strtotime('+6 hours'));
+        $leave = Leave::create($inputs);
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'leave' => $leave,
+            ]
+        ];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Leave  $leave
-     * @return \Illuminate\Http\Response
-     */
     public function show(Leave $leave)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Leave  $leave
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Leave $leave)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Leave  $leave
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Leave $leave)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to create leave category');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Leave  $leave
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Leave $leave)
     {
         //
     }
+
+    private function validateLeave(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'leave_category_id' => 'required|string',
+            'leave_description' => 'required|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+        ]);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
