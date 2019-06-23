c84ae02642aa5cba73f5ba3f14bf1e540c5c3c3c
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-23 18:32:07 +0600

    CORS in simplest form

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,8 @@ class Cors
     {
         // return $next($request);
 
+        return $next($request)->header('Access-Control-Allow-Origin', '*');
+
         // return $next($request)
         //     ->header('Access-Control-Allow-Origin', '*')
         //     ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
