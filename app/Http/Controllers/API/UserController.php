16136e1e1d958a0ece289a1f3a437d3c7844f4d4
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-23 17:42:04 +0600

    only admin can create a user
    Admin can update all infos except password and employee id of any user. Use can update only his/her public information.

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api')->except(['register', 'login']);
+        $this->middleware('auth:api')->except(['login']);
     }
 
     public function login()
@@ -50,8 +50,7 @@ class UserController extends Controller
 
     public function logout(Request $request)
     {
-
-        auth()->user()->token()->revoke(); // after a long way headache, i found this line :)
+        auth()->user()->token()->revoke();
 
         return
         [
@@ -64,22 +63,27 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Only admin can create user');
+
         $validator = Validator::make($request->all(), [
+            'employee_id' => 'required|string',
             'full_name' => 'required|string|min:3|max:25',
             'user_name' => 'required|string|min:3|max:25|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|max:30',
-            'c_password' => 'required|same:password',
+            // 'c_password' => 'required|same:password',
             'date_of_birth' => 'required|date',
+            'fathers_name' => 'required|string|min:3|max:25',
             'gender' => 'required|string',
+            'marital_status' => 'required|string',
             'nationality' => 'required|string',
+            'permanent_address' => 'required|string|min:10|max:300',
+            'present_address' => 'required|string|min:10|max:300',
             'passport_number' => 'required|string',
-            'personal_address' => 'required|string|min:10|max:300',
-            'city' => 'required|string',
             'phone' => 'required|string',
             'designation_id' => 'required|string',
             'department_id' => 'required|string',
-            'working_days_id' => 'required|string',
+            // 'working_days_id' => 'required|string',      // will be done later
             'joining_date' => 'required|date',
         ]);
 
@@ -88,10 +92,7 @@ class UserController extends Controller
             return $this->getErrorMessage($validator->errors());
         }
 
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-
-        $user = User::create($input);
+        $user = User::create($this->getProcessedInputs($request, 1));
         $success['token'] = $user->createToken(config('app.name'))->accessToken;
 
         return
@@ -121,60 +122,66 @@ class UserController extends Controller
 
     public function update(Request $request, $id)
     {
+        $user = Auth::user();
+
+        if($user->isAdmin($user->id) == 'false' and $user->id != $id) return $this->getErrorMessage('Permission denied');
+
         $validator = $this->getValidatorArray($request);
 
-        if ($validator->fails())
+        if($validator->fails())
         {
             return $this->getErrorMessage($validator->errors());
         }
 
-        $user = Auth::user();
         $user_to_be_updated = User::findOrFail($id);
+        $user_to_be_updated->update($this->getProcessedInputs($request, 0));
 
-        if($user->isAdmin($user->id) == 'true')
-        {
-            $input = $request->all();
-            $user_to_be_updated->update($input);
-
-            return
+        return
+        [
             [
-                [
-                    'status' => 'OK',
-                    'message' => 'Info Updated',
-                ]
-            ];
-        }
-
-        if($user->id == $id)
-        {
-            $user_to_be_updated->update($this->getSoftInputs($request));
-
-            return
-            [
-                [
-                    'status' => 'OK',
-                    'message' => 'Soft inputs updated, but restricted inputs did not.',
-                ]
-            ];
-
-        }
-
-        return $this->getErrorMessage('Permission denied');
+                'status' => 'OK',
+                'message' => 'Info Updated',
+            ]
+        ];
     }
 
-    private function getSoftInputs(Request $request)
+    private function getProcessedInputs(Request $request, int $regOrUpdt) // 1 for registration, 0 for update.
     {
         $soft_inputs = [];
 
-        if($request->filled('full_name')) $soft_inputs['full_name'] = $request->input('full_name');
+        if($request->filled('user_name')) $soft_inputs['user_name'] = $request->input('user_name');
         if($request->filled('email')) $soft_inputs['email'] = $request->input('email');
+        if($request->filled('full_name')) $soft_inputs['full_name'] = $request->input('full_name');
         if($request->filled('date_of_birth')) $soft_inputs['date_of_birth'] = $request->input('date_of_birth');
+        if($request->filled('fathers_name')) $soft_inputs['fathers_name'] = $request->input('fathers_name');
         if($request->filled('gender')) $soft_inputs['gender'] = $request->input('gender');
+        if($request->filled('marital_status')) $soft_inputs['marital_status'] = $request->input('marital_status');
         if($request->filled('nationality')) $soft_inputs['nationality'] = $request->input('nationality');
+        if($request->filled('permanent_address')) $soft_inputs['permanent_address'] = $request->input('permanent_address');
+        if($request->filled('present_address')) $soft_inputs['present_address'] = $request->input('present_address');
         if($request->filled('passport_number')) $soft_inputs['passport_number'] = $request->input('passport_number');
-        if($request->filled('personal_address')) $soft_inputs['personal_address'] = $request->input('personal_address');
-        if($request->filled('city')) $soft_inputs['city'] = $request->input('city');
         if($request->filled('phone')) $soft_inputs['phone'] = $request->input('phone');
+
+        if($regOrUpdt == 0 and auth()->user()->isAdmin(auth()->id()) == 'false')
+        {
+            return $soft_inputs;
+        }
+
+        if($request->filled('department_id')) $soft_inputs['department_id'] = $request->input('department_id');
+        if($request->filled('designation_id')) $soft_inputs['designation_id'] = $request->input('designation_id');
+        if($request->filled('working_days_id')) $soft_inputs['working_days_id'] = 0; // will be done later
+        if($request->filled('joining_date')) $soft_inputs['joining_date'] = $request->input('joining_date');
+
+        if($regOrUpdt == 1)
+        {
+            $soft_inputs['employee_id'] = $request->input('employee_id');
+            $soft_inputs['password'] = bcrypt($request->input('password'));
+        }
+
+        else
+        {
+            if($request->filled('status')) $soft_inputs['status'] = $request->input('status');
+        }
 
         return $soft_inputs;
     }
@@ -186,19 +193,18 @@ class UserController extends Controller
             'user_name' => 'string|min:3|max:25|unique:users',
             'email' => 'string|email|max:255|unique:users',
             'date_of_birth' => 'date',
+            'fathers_name' => 'string|min:3|max:25',
             'gender' => 'string',
+            'marital_status' => 'string',
             'nationality' => 'string',
+            'permanent_address' => 'string|min:10|max:300',
+            'present_address' => 'string|min:10|max:300',
             'passport_number' => 'string',
-            'photo_path' => 'string',
-            'personal_address' => 'string|min:10|max:300',
-            'city' => 'string',
             'phone' => 'string',
             'designation_id' => 'string',
             'department_id' => 'string',
-            'salary_id' => 'string',
-            'working_days_id' => 'string',
             'joining_date' => 'date',
-            'status' => 'min:1|max:1'
+            'status' => 'min:1|max:1',
         ]);
     }
 
