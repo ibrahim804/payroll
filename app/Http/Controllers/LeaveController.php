0cfaf29d18336307cbb6843affc8b5146f504299
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-24 15:37:36 +0600

    leave update is partially done

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Leave;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CustomsErrorsTrait;
 use Validator;
@@ -31,19 +32,6 @@ class LeaveController extends Controller
         ];
     }
 
-    public function myLeaves()
-    {
-        $myLeaves = auth()->user()->leaves;
-
-        return
-        [
-            [
-                'status' => 'OK',
-                'myLeaves' => $myLeaves,
-            ]
-        ];
-    }
-
     public function store(Request $request)
     {
         $validate_attributes = $this->validateLeave();
@@ -51,6 +39,7 @@ class LeaveController extends Controller
         $validate_attributes['user_id'] = auth()->id();
         $validate_attributes['application_date'] = date("Y-m-d H:i:s", strtotime('+6 hours'));
         $validate_attributes['month'] = date("M", strtotime('+6 hours'));
+
         $leave = Leave::create($validate_attributes);
 
         return
@@ -62,29 +51,45 @@ class LeaveController extends Controller
         ];
     }
 
-    public function show($id)
+    public function show($user_id)
     {
         if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to view leave');
 
-        $leave = Leave::findOrFail($id);
+        $leaves = User::findOrFail($user_id)->leaves;
 
         return
         [
             [
                 'status' => 'OK',
-                'leave' => $leave,
+                'leave' => $leaves,
             ]
         ];
     }
 
-    public function update(Request $request, Leave $leave)
+    public function update($id) // check end_date >= start_date later
     {
-        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to create leave category');
-    }
+        $leave = Leave::findOrFail($id);
 
-    public function destroy(Leave $leave)
-    {
-        //
+        if($leave->user->id != auth()->id()) return $this->getErrorMessage('You can\'t update others leave information.');
+
+        $validate_attributes = request()->validate([
+            'leave_category_id' => 'string', 'leave_description' => 'string', 'start_date' => 'date', 'end_date' => 'date',
+        ]);
+
+        $leave->update($validate_attributes);
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'user_id' => $leave->user->id,
+                'leave_type' => $leave->leave_category->leave_type,
+                'description' => $leave->leave_description,
+                'application_date' => $leave->application_date,
+                'start_date' => $leave->start_date,
+                'end_date' => $leave->end_date,
+            ]
+        ];
     }
 
     private function validateLeave()
