ad242600f46cfb0549203b0a64a30fe790ee8621
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-25 15:04:25 +0600

    all seven days are fillable. created at and updated_at are hidden

--- app/Working_day.php
+++ app/Working_day.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Working_day extends Model
 {
-    //
+    protected $fillable = [
+        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
 }
