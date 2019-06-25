667358ac4a82053ebd5a0b8a2e51f190aa4a8b46
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-25 15:05:26 +0600

    store method implemented only

--- app/Http/Controllers/WorkingDayController.php
+++ app/Http/Controllers/WorkingDayController.php
@@ -4,82 +4,74 @@ namespace App\Http\Controllers;
 
 use App\Working_day;
 use Illuminate\Http\Request;
+use App\Http\Controllers\CustomsErrorsTrait;
 
 class WorkingDayController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    use CustomsErrorsTrait;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api'); //->except(['register', 'login']);
+    }
+
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Access Denied');
+
+        $validate_attributes = $this->validateWorkingDay();
+        $working_day = Working_day::create($validate_attributes);
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'working_days' => $working_day,
+            ]
+        ];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Working_day  $working_day
-     * @return \Illuminate\Http\Response
-     */
     public function show(Working_day $working_day)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Working_day  $working_day
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Working_day $working_day)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Working_day  $working_day
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Working_day $working_day)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Working_day  $working_day
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Working_day $working_day)
     {
         //
     }
+
+    private function validateWorkingDay()
+    {
+        return request()->validate([
+            'Saturday' => 'required|string',
+            'Sunday' => 'required|string',
+            'Monday' => 'required|string',
+            'Tuesday' => 'required|string',
+            'Wednesday' => 'required|string',
+            'Thursday' => 'required|string',
+            'Friday' => 'required|string',
+        ]);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+//
