bcfca26a00e25b6fb3cd48ad220dc4a062bc6b62
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-25 16:10:20 +0600

    company belongs to a working day

--- app/Company.php
+++ app/Company.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Working_day;
 
 class Company extends Model
 {
@@ -16,10 +17,15 @@ class Company extends Model
         'phone',
         'mobile',
         'website',
-        'working_days_id',
+        'working_day_id',
     ];
 
     protected $hidden = [
         'created_at', 'updated_at',
     ];
+
+    public function working_day()
+    {
+        return $this->belongsTo(Working_day::class);
+    }
 }
