7a7a0d968dda9c158a38c1e40a3caa766db67cc6
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-27 21:56:34 +0600

    few credentials are now fillable while registering a user

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api')->except(['login', 'register']); // remove register from here and ensure only admin is creating user
+        $this->middleware('auth:api')->except(['login']); // remove register from here and ensure only admin is creating user
     }
 
     public function index(Request $request)
@@ -101,27 +101,25 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        // if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Only admin can create user');
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Only admin can create user');
 
         $validator = Validator::make($request->all(), [
-            'employee_id' => 'required|string',
+            'employee_id' => 'string',
             'full_name' => 'required|string|min:3|max:25',
-            'user_name' => 'required|string|min:3|max:25|unique:users',
+            'user_name' => 'string|min:3|max:25|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|max:30',
-            // 'c_password' => 'required|same:password',
-            'date_of_birth' => 'required|date',
-            'fathers_name' => 'required|string|min:3|max:25',
+            'date_of_birth' => 'date',
+            'fathers_name' => 'string|min:3|max:25',
             'gender' => 'required|string',
-            'marital_status' => 'required|string',
-            'nationality' => 'required|string',
-            'permanent_address' => 'required|string|min:10|max:300',
-            'present_address' => 'required|string|min:10|max:300',
-            'passport_number' => 'required|string',
+            'marital_status' => 'string',
+            'nationality' => 'string',
+            'permanent_address' => 'string|min:10|max:300',
+            'present_address' => 'string|min:10|max:300',
+            'passport_number' => 'string',
             'phone' => 'required|string',
-            'designation_id' => 'required|string',
-            'department_id' => 'required|string',
-            // 'working_days_id' => 'required|string',      // will be done later
+            'designation_id' => 'string',
+            'department_id' => 'string',
             'joining_date' => 'required|date',
         ]);
 
@@ -130,7 +128,7 @@ class UserController extends Controller
             return $this->getErrorMessage($validator->errors());
         }
 
-        $user = User::create($this->getProcessedInputs($request, 1));
+        $user = User::create($this->getProcessedInputsWhileCreatingUser($request));
         $success['token'] = $user->createToken(config('app.name'))->accessToken;
 
         return
@@ -138,7 +136,6 @@ class UserController extends Controller
             [
                 'status' => 'OK',
                 'full_name' => $user->full_name,
-                'user_name' => $user->user_name,
                 'email' => $user->email,
                 'token' => $success['token'],
             ]
@@ -183,46 +180,30 @@ class UserController extends Controller
         ];
     }
 
-    private function getProcessedInputs(Request $request, int $regOrUpdt) // 1 for registration, 0 for update.
+    private function getProcessedInputsWhileCreatingUser(Request $request)
     {
-        $soft_inputs = [];
+        $inputs = [];
 
-        if($request->filled('user_name')) $soft_inputs['user_name'] = $request->input('user_name');
-        if($request->filled('email')) $soft_inputs['email'] = $request->input('email');
-        if($request->filled('full_name')) $soft_inputs['full_name'] = $request->input('full_name');
-        if($request->filled('date_of_birth')) $soft_inputs['date_of_birth'] = $request->input('date_of_birth');
-        if($request->filled('fathers_name')) $soft_inputs['fathers_name'] = $request->input('fathers_name');
-        if($request->filled('gender')) $soft_inputs['gender'] = $request->input('gender');
-        if($request->filled('marital_status')) $soft_inputs['marital_status'] = $request->input('marital_status');
-        if($request->filled('nationality')) $soft_inputs['nationality'] = $request->input('nationality');
-        if($request->filled('permanent_address')) $soft_inputs['permanent_address'] = $request->input('permanent_address');
-        if($request->filled('present_address')) $soft_inputs['present_address'] = $request->input('present_address');
-        if($request->filled('passport_number')) $soft_inputs['passport_number'] = $request->input('passport_number');
-        if($request->filled('phone')) $soft_inputs['phone'] = $request->input('phone');
+        $inputs['full_name'] = $request->input('full_name');
+        $inputs['email'] = $request->input('email');
+        $inputs['password'] = bcrypt($request->input('password'));
+        $inputs['gender'] = $request->input('gender');
+        $inputs['phone'] = $request->input('phone');
+        $inputs['joining_date'] = $request->input('joining_date');
 
-        if($regOrUpdt == 0 and auth()->user()->isAdmin(auth()->id()) == 'false')
-        {
-            return $soft_inputs;
-        }
+        if($request->filled('employee_id')) $inputs['employee_id'] = $request->input('employee_id');
+        if($request->filled('user_name')) $inputs['user_name'] = $request->input('user_name');
+        if($request->filled('date_of_birth')) $inputs['date_of_birth'] = $request->input('date_of_birth');
+        if($request->filled('fathers_name')) $inputs['fathers_name'] = $request->input('fathers_name');
+        if($request->filled('marital_status')) $inputs['marital_status'] = $request->input('marital_status');
+        if($request->filled('nationality')) $inputs['nationality'] = $request->input('nationality');
+        if($request->filled('permanent_address')) $inputs['permanent_address'] = $request->input('permanent_address');
+        if($request->filled('present_address')) $inputs['present_address'] = $request->input('present_address');
+        if($request->filled('passport_number')) $inputs['passport_number'] = $request->input('passport_number');
+        if($request->filled('department_id')) $inputs['department_id'] = $request->input('department_id');
+        if($request->filled('designation_id')) $inputs['designation_id'] = $request->input('designation_id');
 
-        if($request->filled('department_id')) $soft_inputs['department_id'] = $request->input('department_id');
-        if($request->filled('designation_id')) $soft_inputs['designation_id'] = $request->input('designation_id');
-        if($request->filled('joining_date')) $soft_inputs['joining_date'] = $request->input('joining_date');
-
-        $soft_inputs['working_days_id'] = 0; // WILL BE DONE LATER
-
-        if($regOrUpdt == 1)
-        {
-            $soft_inputs['employee_id'] = $request->input('employee_id');
-            $soft_inputs['password'] = bcrypt($request->input('password'));
-        }
-
-        else
-        {
-            if($request->filled('status')) $soft_inputs['status'] = $request->input('status');
-        }
-
-        return $soft_inputs;
+        return $inputs;
     }
 
     private function getValidatorArray(Request $request)
