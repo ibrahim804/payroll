b02411a9ce00113c187b09ffeed673f1c9ba0e9c
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-27 11:29:59 +0600

    use soft delete

--- app/Company.php
+++ app/Company.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Working_day;
 
 class Company extends Model
 {
-    //
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
