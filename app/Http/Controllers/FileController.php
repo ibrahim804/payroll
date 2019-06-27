2598b7b0d75d4139ed32fd194d5d4bf42f25bfc1
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-27 14:24:01 +0600

    File Controller added for managing file

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    public function upload(Request $request)
+    {
+        $validate_attributes = $this->validateFile();
+        $file_name = 'something.'.$request->input('type');
+        $file_path = url('/files'.'/'.$file_name);
+        $request->file('file')->move(public_path('/files'), $file_name);
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'url' => $file_path,
+            ]
+        ];
+    }
+
+    private function validateFile()
+    {
+        return request()->validate([
+            'file' => 'required|file',
+            'type' => 'required|string',
+        ]);
+    }
+}
