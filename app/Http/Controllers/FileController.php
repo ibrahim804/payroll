dde65669eeecb397ba0cf220586fa41cbfb5e350
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-27 15:45:20 +0600

    add auth in file controller

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\CustomsErrorsTrait;
 
 class FileController extends Controller
 {
+    use CustomsErrorsTrait;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api'); //->except(['register', 'login']);
+    }
+
     public function upload(Request $request)
     {
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('You don\'t have permission to upload file');
+
         $validate_attributes = request()->validate(['file' => 'required|file|max:5120']);
 
         $file_name = $request->file('file')->getClientOriginalName();
