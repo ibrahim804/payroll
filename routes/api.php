dd9fd75dccfe02ccecc374a361d53e33182572f1
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-27 11:53:49 +0600

    refactored

--- routes/api.php
+++ routes/api.php
@@ -39,7 +39,7 @@ Route::middleware('cors')->group(function(){
 
     Route::get('companies', 'CompanyController@index');
     Route::get('companies/trashed', 'CompanyController@trashedIndex');
-    Route::post('company', 'CompanyController@store');                  // Need to be implemented more
+    Route::post('company', 'CompanyController@store');
     Route::get('company/{id}', 'CompanyController@show');
     Route::post('company/{id}', 'CompanyController@update');
     Route::get('company/delete/{id}', 'CompanyController@destroy');
