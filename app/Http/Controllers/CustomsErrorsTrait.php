9b7e90cd88c519549a02a04947dc9325221ea602
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-30 17:32:38 +0600

    user validation is checked now from CustomErrorTrait

--- app/Http/Controllers/CustomsErrorsTrait.php
+++ app/Http/Controllers/CustomsErrorsTrait.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Validator;
 
 trait CustomsErrorsTrait
 {
@@ -15,4 +16,27 @@ trait CustomsErrorsTrait
             ]
         ];
     }
+
+	public function validateUser($inputs)
+	{
+		return Validator::make($inputs, [
+            'employee_id' => 'string',
+            'full_name' => 'required|string|min:3|max:25',
+            'user_name' => 'string|min:3|max:25|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|max:30',
+            'date_of_birth' => 'date',
+            'fathers_name' => 'string|min:3|max:25',
+            'gender' => 'required|string',
+            'marital_status' => 'string',
+            'nationality' => 'string',
+            'permanent_address' => 'string|min:10|max:300',
+            'present_address' => 'string|min:10|max:300',
+            'passport_number' => 'string',
+            'phone' => 'required|string',
+            'designation_id' => 'string',
+            'department_id' => 'string',
+            'joining_date' => 'required|date',
+        ]);
+	}
 }
