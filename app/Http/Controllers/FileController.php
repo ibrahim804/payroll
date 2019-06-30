48467325141fbdaa96148b4426961ac9e67ada95
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-30 15:32:03 +0600

    user create from csv file done without validation

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\CustomsErrorsTrait;
+use App\User;
 
 class FileController extends Controller
 {
@@ -11,7 +12,7 @@ class FileController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api'); //->except(['register', 'login']);
+        $this->middleware('auth:api');
     }
 
     public function upload(Request $request)
@@ -21,19 +22,55 @@ class FileController extends Controller
         $validator = request()->validate(['file' => 'required|file']); // required|file|max:5120, example of multiple mimes:csv,txt
         $file = $request->file('file');
 
-        if($file->getClientOriginalExtension() != 'csv') return $this->getErrorMessage('you uploaded a '.$file->getClientOriginalExtension().' file, only csv allowed.');
+        if($file->getClientOriginalExtension() != 'csv')
+        {
+            return $this->getErrorMessage('you uploaded a '.$file->getClientOriginalExtension().' file, only csv is allowed.');
+        }
+
+        /*
+        IF WE WANT TO STORE THE REQUESTED FILE TO PUBLIC FOLDER.
 
         $file_name = $file->getClientOriginalName();
         $file_path = url('/files'.'/'.$file_name);
-
         $request->file('file')->move(public_path('/files'), $file_name);
 
-        return
-        [
+        // or to retrieve the file from public folder,     $file = public_path('files/test.csv');
+        */
+
+        return $this->csvToArrayToDatabase($file);
+    }
+
+    private function csvToArrayToDatabase($filename = '', $delimiter = ',')
+    {
+        if (!file_exists($filename) || !is_readable($filename)) return $this->getErrorMessage('File is not readable.');
+
+        $header = null;
+        // $data = array();
+
+        if (($handle = fopen($filename, 'r')) !== false)
+        {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false)
+            {
+                if (!$header) $header = $row;
+
+                else
+                {
+                    // $data[] = array_combine($header, $row);
+                    User::create(array_combine($header, $row));
+                }
+            }
+
+            fclose($handle);
+            // return $date;
+            return
             [
-                'status' => 'OK',
-                'url' => $file_path,
-            ]
-        ];
+                [
+                    'status' => 'OK',
+                    'message' => 'User from uploaded file created successfully.',
+                ]
+            ];
+        }
+
+        return $this->getErrorMessage('File can\'t be open.');
     }
 }
