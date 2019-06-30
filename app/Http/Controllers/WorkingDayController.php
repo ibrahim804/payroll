e87e5e2200351ede964ddd5f385baeb99ab176f4
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-30 12:35:37 +0600

    working day controller done

--- app/Http/Controllers/WorkingDayController.php
+++ app/Http/Controllers/WorkingDayController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Working_day;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CustomsErrorsTrait;
+use App\User;
 
 class WorkingDayController extends Controller
 {
@@ -15,16 +16,11 @@ class WorkingDayController extends Controller
         $this->middleware('auth:api'); //->except(['register', 'login']);
     }
 
-    public function index()
-    {
-        
-    }
-
     public function store(Request $request)
     {
         if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Permission Denied');
 
-        $validate_attributes = $this->validateWorkingDay();
+        $validate_attributes = $this->validateWorkingDay(1);
         $working_day = Working_day::create($validate_attributes);
 
         return
@@ -37,31 +33,48 @@ class WorkingDayController extends Controller
         ];
     }
 
-    public function show(Working_day $working_day)
+    public function show($id)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false' and auth()->id() != $id) return $this->getErrorMessage('Permission Denied');
+
+        $working_day = User::findOrFail($id)->working_day;
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'working_day' => $working_day,
+            ]
+        ];
     }
 
-    public function update(Request $request, Working_day $working_day)
+    public function update($id)
     {
-        //
+        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Only admin can update one\'s working day info');
+
+        $working_day = User::findOrFail($id)->working_day;
+        $validate_attributes = $this->validateWorkingDay(0);
+        $working_day->update($validate_attributes);
+
+        return
+        [
+            [
+                'status' => 'OK',
+                'working_day' => $working_day, 
+            ]
+        ];
     }
 
-    public function destroy(Working_day $working_day)
-    {
-        //
-    }
-
-    private function validateWorkingDay()
+    private function validateWorkingDay(int $StoreOrUpdate) // 1 for store, 0 for update
     {
         return request()->validate([
-            'saturday' => 'required|string',
-            'sunday' => 'required|string',
-            'monday' => 'required|string',
-            'tuesday' => 'required|string',
-            'wednesday' => 'required|string',
-            'thursday' => 'required|string',
-            'friday' => 'required|string',
+            'saturday' => ($StoreOrUpdate) ? 'required|string' : 'string',
+            'sunday' => ($StoreOrUpdate) ? 'required|string' : 'string',
+            'monday' => ($StoreOrUpdate) ? 'required|string' : 'string',
+            'tuesday' => ($StoreOrUpdate) ? 'required|string' : 'string',
+            'wednesday' => ($StoreOrUpdate) ? 'required|string' : 'string',
+            'thursday' => ($StoreOrUpdate) ? 'required|string' : 'string',
+            'friday' => ($StoreOrUpdate) ? 'required|string' : 'string',
         ]);
     }
 }
