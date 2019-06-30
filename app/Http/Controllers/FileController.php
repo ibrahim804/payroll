7cd9d95254bac49c8ef3ac1c186e766185199c37
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-06-30 16:51:55 +0600

    file data are now checked through laravel built in validator

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CustomsErrorsTrait;
 use App\User;
+use Validator;
+use App\MyErrorObject;
 
 class FileController extends Controller
 {
@@ -44,8 +46,7 @@ class FileController extends Controller
     {
         if (!file_exists($filename) || !is_readable($filename)) return $this->getErrorMessage('File is not readable.');
 
-        $header = null;
-        // $data = array();
+        $header = null; $row_count = 1; $error_index = 0; $myCustomErrors = [];
 
         if (($handle = fopen($filename, 'r')) !== false)
         {
@@ -55,22 +56,58 @@ class FileController extends Controller
 
                 else
                 {
-                    // $data[] = array_combine($header, $row);
-                    User::create(array_combine($header, $row));
+                    $single_row = array_combine($header, $row);
+                    $validator = $this->validateUser($single_row);
+
+                    if ($validator->fails())
+                    {
+                        $myCustomErrors[] = new MyErrorObject;
+                        $myCustomErrors[$error_index]->id = $row_count;
+                        $myCustomErrors[$error_index]->error = $validator->errors();
+                        $error_index++;
+                    }
+
+                    else User::create($single_row);
                 }
+
+                $row_count++;
             }
 
             fclose($handle);
-            // return $date;
+
             return
             [
                 [
                     'status' => 'OK',
                     'message' => 'User from uploaded file created successfully.',
+                    'errors' => $myCustomErrors,
                 ]
             ];
         }
 
         return $this->getErrorMessage('File can\'t be open.');
+    }
+
+    private function validateUser($single_row)
+    {
+        return Validator::make($single_row, [
+            'employee_id' => 'string',
+            'full_name' => 'required|string|min:3|max:25',
+            'user_name' => 'string|min:3|max:25|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|max:30',
+            'date_of_birth' => 'date',
+            'fathers_name' => 'string|min:3|max:25',
+            'gender' => 'required|string',
+            'marital_status' => 'string',
+            'nationality' => 'string',
+            'permanent_address' => 'string|min:10|max:300',
+            'present_address' => 'string|min:10|max:300',
+            'passport_number' => 'string',
+            'phone' => 'required|string',
+            'designation_id' => 'string',
+            'department_id' => 'string',
+            'joining_date' => 'required|date',
+        ]);
     }
 }
