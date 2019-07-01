423bf3c5e9e02ae4b436cd98db6494cdf7dc96a8
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-07-01 14:43:12 +0600

    fillable property added. No relation added with user and leave category

--- app/LeaveCount.php
+++ app/LeaveCount.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+// Query will be enough bigger, so no need to make any relation with user and leave category.
+
 class LeaveCount extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'leave_category_id',
+        'leave_left',
+        'leave_count_start',
+        'leave_count_expired',
+    ];
 }
