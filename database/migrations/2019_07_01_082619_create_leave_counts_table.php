d764234300bac4142cb1bf0836fb64e392048539
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-07-01 14:28:17 +0600

    leave count management table added

---
+++ database/migrations/2019_07_01_082619_create_leave_counts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaveCountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave_counts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('leave_category_id');
+            $table->integer('leave_left');
+            $table->date('leave_count_start');
+            $table->date('leave_count_expired');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_counts');
+    }
+}
