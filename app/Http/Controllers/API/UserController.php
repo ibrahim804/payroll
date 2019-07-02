efc751a546997c7307c7dabfcc982bead12071b2
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-07-02 21:24:38 +0600

    register method has no auth (temporary)

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api')->except(['login']);
+        $this->middleware('auth:api')->except(['login', 'register']);
     }
 
     public function index(Request $request)
@@ -95,7 +95,7 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Only admin can create user');
+        // if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Only admin can create user');
 
         $validator = $this->validateUser($request->all());
 
