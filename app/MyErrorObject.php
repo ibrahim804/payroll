352ae5584e0d6fa20782a6eac69c764ccdbd9bd1
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-07-06 10:50:01 +0600

    profile pictures and trashed pictures directory are listed in my object

--- app/MyErrorObject.php
+++ app/MyErrorObject.php
@@ -5,4 +5,6 @@ namespace App;
 class MyErrorObject
 {
     public $id, $error;
+    public $profile_pictures = '/images/profile_pictures';
+    public $trashed_pictures = '/images/trashed_pictures';
 }
