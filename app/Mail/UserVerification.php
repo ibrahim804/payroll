591c9b95a3bc62c8680ff5fb098ad5c057faa4d8
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-07-10 14:57:33 +0600

    Mail ca be sent now through this class

---
+++ app/Mail/UserVerification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserVerification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $code, $full_name;
+
+    public function __construct($code, $full_name)
+    {
+        $this->code = $code;
+        $this->full_name = $full_name;
+    }
+
+
+    public function build()
+    {
+        return $this->subject('Request for password reset')
+                    ->markdown('mail.forgot-password');
+    }
+}
