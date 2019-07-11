a3b5035d0430017def170d38b88f02e94bf34a29
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-07-11 16:49:31 +0600

    no button added in mail template

--- resources/views/mail/forgot-password.blade.php
+++ resources/views/mail/forgot-password.blade.php
@@ -8,9 +8,5 @@ Your account verification code for password recovery is given below
      <b> {{ $code }} </b>
 </div
 
-@component('mail::button', ['url' => url('/api')]) <!-- url will be a get request of frontend, then the flow will call a post request of backend. -->
-Set a new password now
-@endcomponent
-
 Thank you. <br>
 @endcomponent
