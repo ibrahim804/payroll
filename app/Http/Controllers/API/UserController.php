e814238b779f606f6c5f22db159faa8d3acff54c
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-07-31 12:51:15 +0600

    Commented UserController

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -15,6 +15,12 @@ use Carbon\Carbon;
 
 class UserController extends Controller
 {
+
+    /*
+        CustomsErrorsTrait is a trait which contains some methods. Evety Controller that uses CustomsErrorsTrait can access all it's methods.
+         To access some methods globally, we generally create a trait and write function there.
+    */
+
     use CustomsErrorsTrait;
 
     public $successStatus = 200;
@@ -22,13 +28,22 @@ class UserController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api')->except(['login', 'forgot_password', 'verifyVerificationCode', 'setNewPasswordAfterUserVerification']);
+
+        /* To allow some specific methods instead, we use "only".
+            For example: $this->middleware('auth:api')->only(['index', logout]);
+            Both only and except can't be used within a same Controller.
+        */
     }
+
+    /*
+        returns all users' id, full_name, payable salary, company name, department, designation, casual_leave left, sick_leave left, photo_path,
+    */
 
     public function index(Request $request)
     {
         if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Permission denied');
 
-        $users = User::all();
+        $users = User::all();               // fetches all users
         $i = 0; $infos = [];
 
         foreach($users as $user) {
@@ -57,6 +72,10 @@ class UserController extends Controller
         ];
     }
 
+    /*
+        Takes salary object(record) and retrurns payable amount
+    */
+
     private function calculateNetSalary($salary)
     {
         return $salary['basic_salary']
@@ -66,12 +85,17 @@ class UserController extends Controller
             -  $salary['tax_deduction'] -  $salary['provident_fund'] - $salary['other_deduction'];
     }
 
+    /*
+        Input: email and password
+        returns full_name, email and token.
+    */
+
     public function login()
     {
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')]))
         {
-            $user = Auth::user();
-            $success['token'] = $user->createToken(config('app.name'))->accessToken;
+            $user = Auth::user();   // user is now logged in
+            $success['token'] = $user->createToken(config('app.name'))->accessToken;    // create a token
 
             return
             [
@@ -84,13 +108,17 @@ class UserController extends Controller
             ];
         }
 
+        /*
+            getErrorMessage is a method under CustomsErrorsTrait. As, this Controller uses CustomsErrorsTrait, it get's access of this method.
+        */
+
         return $this->getErrorMessage('credentials are not matched.');
 
     }
 
     public function logout(Request $request)
     {
-        auth()->user()->token()->revoke();
+        auth()->user()->token()->revoke();      // The token is now no longer available.
 
         return
         [
@@ -105,15 +133,20 @@ class UserController extends Controller
     {
         if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Only admin can create user');
 
-        $validator = $this->validateUser($request->all());
+        $validator = $this->validateUser($request->all());          // validateUser is a method of CustomsErrorsTrait. Please check
 
         if ($validator->fails())
         {
-            return $this->getErrorMessage($validator->errors());
+            return $this->getErrorMessage($validator->errors());    // getErrorMessage is a method of CustomsErrorsTrait
         }
 
+        /*
+            About user creation:
+                All parameters that admin fills out are not required. It must be checked whether not-required fields are filled out or not.
+        */
+
         $user = User::create($this->getProcessedInputsWhileCreatingUser($request));
-        $success['token'] = $user->createToken(config('app.name'))->accessToken;
+        $success['token'] = $user->createToken(config('app.name'))->accessToken;    // syntex
 
         return
         [
@@ -126,6 +159,10 @@ class UserController extends Controller
         ];
     }
 
+    /*
+        User can view only her/his user onfo, none else. But admin can
+    */
+
     public function user($id)
     {
         if(auth()->user()->isAdmin(auth()->id()) == 'false' and auth()->id() != $id)
@@ -133,7 +170,7 @@ class UserController extends Controller
             return $this->getErrorMessage('You don\'t have permission to view any user info');
         }
 
-        $user = User::findOrFail($id);
+        $user = User::findOrFail($id);              // if not found, returns an exception
 
         return
         [
@@ -144,9 +181,13 @@ class UserController extends Controller
         ];
     }
 
+    /*
+        Specific user can update her/his info. But admin can anyone's.
+    */
+
     public function update(Request $request, $id)
     {
-        $user = Auth::user();
+        $user = Auth::user();                       // catch the user
 
         if($user->isAdmin($user->id) == 'false' and $user->id != $id) return $this->getErrorMessage('Permission denied');
 
@@ -165,8 +206,12 @@ class UserController extends Controller
             'phone' => 'string',
         ]);
 
-        $user_to_be_updated = User::findOrFail($id);
-        $user_to_be_updated->update($validate_attributes);
+        $user_to_be_updated = User::findOrFail($id);                // finding user we're going to update
+        $user_to_be_updated->update($validate_attributes);          // update with required params
+
+        /*
+            User can't update the following infos, but admin can.
+        */
 
         if($user->isAdmin($user->id) == 'true')
         {
@@ -191,9 +236,21 @@ class UserController extends Controller
         ];
     }
 
+    /*
+        For not-required parameters: If any parameter is filled out, it will be stored.
+    */
+
     private function getProcessedInputsWhileCreatingUser(Request $request)
     {
         $inputs = [];
+
+        /*
+            We can insert values in $inputs array in the following way,
+
+            $inputs = $request->all();
+
+            But the problem is, some unexpected parameters can be passed. But out list is specific.
+        */
 
         $inputs['full_name'] = $request->input('full_name');
         $inputs['email'] = $request->input('email');
@@ -218,6 +275,10 @@ class UserController extends Controller
         return $inputs;
     }
 
+    /*
+        The following route will be called when user knows her/his password but wants to update
+    */
+
     public function change_password(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -233,7 +294,7 @@ class UserController extends Controller
 
         if(Auth::guard('web')->attempt(['id' => auth()->id(), 'password' => $request->input('current_password')]))
         {
-            $new_password = bcrypt($request->input('new_password'));
+            $new_password = bcrypt($request->input('new_password'));        // new_password should be hashed beforing storing
             auth()->user()->update(['password' => $new_password]);
 
             return
@@ -248,6 +309,12 @@ class UserController extends Controller
         return $this->getErrorMessage('Current password is not correct.');
     }
 
+    /*
+        Hits when user forgets her/his password. User must provide her/his email to get mail.
+        1. System sends a verification code to user's mail.
+        2. Store that verification code to user's database for later verification
+    */
+
     public function forgot_password(Request $request)
     {
         $validate_attributes = request()->validate(['email' => 'required|string']);
@@ -255,13 +322,13 @@ class UserController extends Controller
 
         if(! $user) return $this->getErrorMessage('User with this email doesn\'t exist');
 
-        $verification_code = mt_rand(100000, 999999);
+        $verification_code = mt_rand(100000, 999999);           // generate a 6 digit code
 
         Mail::to($user->email)->send(
             new UserVerification($verification_code, $user->full_name, ($user->company) ? $user->company->name : '')
         );
 
-        $user->update(['verification_code' => $verification_code]);
+        $user->update(['verification_code' => $verification_code]);     // verification_code is stored in user database for verify this user later
 
         return
         [
@@ -271,6 +338,18 @@ class UserController extends Controller
             ]
         ];
     }
+
+    /*
+        Following route will be called after receiving verification code
+        Input: take email for querying verification code from database
+
+        Then user submit code here what s/he received to set new password. The field for verification code of database can be,
+        1. NULL (reasons are: code is not sent successfully through mail, or code gets expired already. Note that, code gets expired when it is one minute old)
+        2. 6 digit number (Code is sent to mail, but not verified yet.)
+        3. 1 (When user is already verified)
+
+        Output: Ensures that user is now allowed to set a new password
+    */
 
     public function verifyVerificationCode(Request $request)
     {
@@ -294,16 +373,16 @@ class UserController extends Controller
             ];
         }
 
-        $user_updated_at = Carbon::createFromFormat('Y-m-d H:i:s', $user->updated_at);
+        $user_updated_at = Carbon::createFromFormat('Y-m-d H:i:s', $user->updated_at);          // when verification code is created.
         $current_time = Carbon::createFromFormat('Y-m-d H:i:s', Carbon::now());
-        $ageOfVerificationCode = $user_updated_at->diffInSeconds($current_time);
+        $ageOfVerificationCode = $user_updated_at->diffInSeconds($current_time);                // get the age of verification code
 
-        if($ageOfVerificationCode >= 60) $user->update(['verification_code' => NULL]);
+        if($ageOfVerificationCode >= 60) $user->update(['verification_code' => NULL]);          // verification code get expired after one minute
 
         if(! $user->verification_code) return $this->getErrorMessage('Either you didn\'t send forgot password request, or your code expired');
         if($validate_attributes['verification_code'] != $user->verification_code) return $this->getErrorMessage('Verification code isn\'t matched');
 
-        $user->update(['verification_code' => 1]);
+        $user->update(['verification_code' => 1]);                                              // OK, user is not verified and allowed to set new password
 
         return
         [
@@ -314,6 +393,11 @@ class UserController extends Controller
         ];
 
     }
+
+    /*
+        The following route will be called when user is already verified through verification acode and ready to set a new password
+        Input: email, new_password and confirm_password
+    */
 
     public function setNewPasswordAfterUserVerification(Request $request)
     {
@@ -326,10 +410,10 @@ class UserController extends Controller
         $user = User::where('email', $validate_attributes['email'])->first();
 
         if(! $user) return $this->getErrorMessage('User with this email doesn\'t exist');
-        if($user->verification_code != 1) return $this->getErrorMessage('Please verify your verification code firstly to set new password');
+        if($user->verification_code != 1) return $this->getErrorMessage('Please verify your verification code firstly to set new password'); // No permission if code is not verified yet.
 
         $user->update([
-            'password' => bcrypt($validate_attributes['new_password']),
+            'password' => bcrypt($validate_attributes['new_password']),             // password is hashed
             'verification_code' => NULL,
         ]);
 
@@ -341,6 +425,8 @@ class UserController extends Controller
             ]
         ];
     }
+
+    // SoftDelete
 
     public function delete(Request $request, $id)
     {
@@ -361,7 +447,7 @@ class UserController extends Controller
     {
         if(auth()->user()->isAdmin(auth()->id()) == 'false') return $this->getErrorMessage('Permission denied for normal user');
 
-        User::onlyTrashed()->where('id', $id)->restore();
+        User::onlyTrashed()->where('id', $id)->restore();                       // Deleted user can be restored when it is trashed.
 
         return
         [
@@ -372,30 +458,35 @@ class UserController extends Controller
         ];
     }
 
+    /* If user wants to remove her/his profile picture
+        Their are two directories /public/images. one is profile_pictures(all profile pictures are here), another is trashed_pictures(all removed profile pictures are here)
+        So when user remove photo, the system basically move profile picture from profile_pictures directory to trashed_pictures directory
+    */
+
     public function remove_photo($id)
     {
-        if(auth()->user()->isAdmin(auth()->id()) == 'false' and auth()->id() != $id)
+        if(auth()->user()->isAdmin(auth()->id()) == 'false' and auth()->id() != $id)    // User can remove her/his profile picture, Admin can anyone's
         {
             return $this->getErrorMessage('Permission denied.');
         }
 
         $user = User::findOrFail($id);
 
-        if(!$user->photo_path) return $this->getErrorMessage('No photo has been found in this path.');
+        if(!$user->photo_path) return $this->getErrorMessage('No photo has been found in this path.');      // If photo exists, path must have a value
 
-        $actual_old_path = public_path($user->photo_path);
-        $extension = pathinfo($actual_old_path, PATHINFO_EXTENSION);   // Simple way of finding extension from path string
-        $actual_new_path = public_path().(new \App\MyErrorObject)->trashed_pictures.'/'.bin2hex(random_bytes(8)).'.'.$extension;
+        $actual_old_path = public_path($user->photo_path);              // physical path where the photo is
+        $extension = pathinfo($actual_old_path, PATHINFO_EXTENSION);    // extension is needed to rename that profile picture in trashed_pictures directory,
+        $actual_new_path = public_path().(new \App\MyErrorObject)->trashed_pictures.'/'.bin2hex(random_bytes(8)).'.'.$extension;        // where to save(trashed_pictures location)
 
         /* Another way of finding extension from path string
             $infoPath = pathinfo(public_path('/uploads/my_image.jpg'));
             $extension = $infoPath['extension'];
         */
 
-        if(! File::exists($actual_old_path)) return $this->getErrorMessage('File doesn\'t exists in this path');
+        if(! File::exists($actual_old_path)) return $this->getErrorMessage('File doesn\'t exists in this path');    // before moving, we need to ensure that, photo really exists in this actual path
 
-        File::move($actual_old_path, $actual_new_path);
-        $user->update(['photo_path' => NULL]);
+        File::move($actual_old_path, $actual_new_path);     // profile_pictures to trashed_pictures
+        $user->update(['photo_path' => NULL]);              // No photo in profile_pictures directory
 
         return
         [
